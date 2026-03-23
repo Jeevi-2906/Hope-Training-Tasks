6b3bfecd7c782ba6be2f6da34bb944499c44c95f
Jeevi-2906 <jeevikak.1029@gmail.com>
2026-03-23 18:07:11 +0530

    Create leetcode-190.java

---
+++ leetcode-190.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+        for(int i=0;i<32;i++)
+        {
+            if((n& 1)==1)
+            {
+                ans = ans | 1 << (31-i);
+            }
+            n>>=1;
+        }
+        return ans;
+    }
+}
