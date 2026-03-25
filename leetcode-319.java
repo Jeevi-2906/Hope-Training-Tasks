d177349cbaf607c8431d774ee23fdb87650caf9d
Jeevika <jeevikak.1029@gmail.com>
2026-03-25 10:22:53 +0530

    Add DAY 1-5 training folders

--- leetcode-319.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public int bulbSwitch(int n) {
-        if(n<=0) return 0;
-        return (int) Math.sqrt(n);
-    }
-}
--- leetcode-342.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public boolean isPowerOfFour(int n) {
-        if((n&(n-1)) == 0 && (n&0x55555555)!=0) return true;
-        else return false;
-    }
-}
